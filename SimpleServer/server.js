d46c60b1f77d81931881c0f7cdbc1ef793362464
dectony <tony.zasenko@gmail.com>
2019-02-28 17:45:53 +0300

    some improvments

--- SimpleServer/server.js
+++ SimpleServer/server.js
@@ -1,9 +1,22 @@
 const express  = require('express');
 const app = express();
 
+let degrees = 0
+
 app.get('/', (req,res) => {
     res.send('Hello');
-    console.log('Got reauest from' + req.ip);
+    console.log('Got request from' + req.ip);
 })
+
+app.get('/temp/:degrees', (req,res) => {
+    res.send(req.params);
+    degrees = req.params.degrees;
+    console.log('Set temp to '+ degrees);
+});
+
+app.get('/temp', (req,res) => {
+    res.send('Current temperature is '+ degrees);
+    console.log('Current temperature is '+ degrees);
+});
 
 app.listen(3000, () => console.log('Server is running'));

728c795a3d65b0559b9137f3b42801fe7b965697
Anton Zasenka <tony.zasenko@gmail.com>
2019-03-11 22:36:47 +0300

    mqtt forwarding added

--- SimpleServer/server.js
+++ SimpleServer/server.js
@@ -1,7 +1,10 @@
 const express  = require('express');
+const mqtt = require('mqtt');
 const app = express();
 
 let degrees = 0
+
+var mqttClient = mqtt.connect('http://broker.hivemq.com');
 
 app.get('/', (req,res) => {
     res.send('Hello');
@@ -12,6 +15,15 @@ app.get('/temp/:degrees', (req,res) => {
     res.send(req.params);
     degrees = req.params.degrees;
     console.log('Set temp to '+ degrees);
+});
+
+app.get('/topic/:topic/value/:topicValue', (req,res) => {
+    let topic = req.params.topic;
+    let value = req.params.topicValue;
+
+    res.send(req.params);
+    mqttClient.publish(topic, value)
+    console.log('topic/value :'+ topic+'/'+value);
 });
 
 app.get('/temp', (req,res) => {
